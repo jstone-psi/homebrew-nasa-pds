951d80f0aa6fd2caea2968f4c56cb28f470c6a05
Jesse Stone <jstone@psi.edu>
2019-06-19 11:16:54 -0700

    Updated for PDS4 Viewer 1.1

--- Casks/pds4-viewer.rb
+++ Casks/pds4-viewer.rb
@@ -1,10 +1,22 @@
 cask 'pds4-viewer' do
-  version '0.9'
-  sha256 'f4c2c8d998b784e38ddb6d9e736011340a662c63b352a8d8e34ebf3e97f74a38'
 
-  url 'http://pdssbn.astro.umd.edu/ftp/tools/pds4_viewer/0.9/PDS4_viewer_mac-0.9.zip'
+  if MacOS.version <= '10.12'
+    version '1.1'
+    sha256 'f91f94658efc7b8cd7031e4220c2ed55bfb0a04eb055afcced661e1c28443393'
+
+    url 'https://pdssbn.astro.umd.edu/ftp/tools/pds4_viewer/1.1/PDS4_viewer_mac10_6-1.1.zip'
+
+    app 'pds4_viewer.app'
+  else
+    version '1.1'
+    sha256 '2523f3381deb016cf90de055c280ea341923b807043c02acd1c4faa8929972a4'
+
+    url 'https://pdssbn.astro.umd.edu/ftp/tools/pds4_viewer/1.1/PDS4_viewer_mac10_13-1.1.zip'
+
+    app 'pds4_viewer.app'
+  end
+
   name 'PDS4 Viewer'
   homepage 'http://sbndev.astro.umd.edu/wiki/PDS4_Viewer'
 
-  app 'pds4_viewer.app'
 end
