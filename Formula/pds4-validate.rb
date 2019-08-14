38ee7ce6a6bfcd7f6f1f62718353facf29c20cbb
Jesse Stone <jstone@psi.edu>
2019-08-14 15:29:12 -0700

    Updated to validate 1.15

--- Formula/pds4-validate.rb
+++ Formula/pds4-validate.rb
@@ -1,10 +1,10 @@
 class Pds4Validate < Formula
   desc "PDS4 Label Validation Tool"
-  homepage "https://pds.nasa.gov/pds4/software/validate/"
-  url "ftp://pds.nasa.gov/pub/toplevel/2010/preparation/validate-1.13.0-bin.zip"
-  sha256 "38f9a56a60d9047ab876bf3b1ae67a2866053ed8230e9a902ef72c5df635310b"
+  homepage "https://pds-engineering.jpl.nasa.gov/development/pds4/current/preparation/validate/index.html"
+  url "https://pds.nasa.gov/software/pds4/preparation/validate-1.15.0-bin.tar.gz"
+  sha256 "f2961c168e4367c7b3da77a25eaa7d2e88619181c4f8ad746d05eee32b4d4d6e"
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8+"
 
   def install
     rm Dir["bin/*.{bat,cmd,dll,exe}"]
